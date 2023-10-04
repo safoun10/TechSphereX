01e1b55405dd6b9c4f242ab8e430f82fb7d03ef9
Safoun <safoun.official@gmail.com>
2023-10-04 11:05:19 +0600

    still setting up project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div className="display-6">Hello world!</div>,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
