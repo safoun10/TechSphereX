e044a81c76cd97fb675bb32771905e4721b82b9a
Safoun <safoun.official@gmail.com>
2024-01-03 21:32:36 +0600

    adding codes for commit, will fix later

--- src/components/banner/dream/Dream.jsx
+++ src/components/banner/dream/Dream.jsx
@@ -6,7 +6,16 @@ const Dream = () => {
 			<div className="col-11 col-md-5">photo</div>
 			<div className="col-11 col-md-7 text-end">
 				<p>Build your dream with us</p>
-				<div className="btn btn-outline-dark border-3 fs-5 py-1 px-4">join now</div>
+				<div className="">
+					<img
+						className="img-fluid w-25 border"
+						src="https://i.postimg.cc/Px4gyh3S/proper-techspherex-without-bg-Photo-Room-png-Photo-Room.png"
+					/>
+					{/* <img className="img-fluid w-25 border" src="https://i.postimg.cc/BQ3qnKqW/Screenshot-2023-10-10-203023-removebg-preview-3.png" />  */}
+				</div>
+				<div className="btn btn-outline-dark border-3 fs-5 py-1 px-4">
+					join now
+				</div>
 			</div>
 		</div>
 	);
